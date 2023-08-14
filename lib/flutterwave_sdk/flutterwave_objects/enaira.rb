8a137b46e7ba8b6098fd67d9f8a6eb7d212c7e4e
Ifunanya Ikemma <ifavour902@gmail.com>
2023-08-14 12:57:53 +0100

    add enaira feature to the sdk

--- lib/flutterwave_sdk/flutterwave_objects/enaira.rb
+++ lib/flutterwave_sdk/flutterwave_objects/enaira.rb
@@ -1,7 +1,7 @@
 require_relative "base/base.rb"
 require 'json'
 
-class UkPayment < Base
+class ENairaPayment < Base
     def initiate_charge(data)
         base_url = flutterwave_object.base_url
 
@@ -9,20 +9,10 @@ class UkPayment < Base
         if !data.key?("tx_ref")
             data.merge!({"tx_ref" => Util.transaction_reference_generator})
         end
-        # check the currency to determine the type and the required parameters
-        currency = data["currency"]
-        if currency == "GBP"
-            required_parameters =  [ "amount", "email",  "tx_ref", "currency", "is_token_io"]
-            type = "account-ach-uk"
-        elsif currency  == "EUR"
-            required_parameters =  [ "amount", "email", "tx_ref", "currency", "is_token_io"]
-            type = "account-ach-uk"
-        else
-            return "pass a valid currency"
-        end
+            required_parameters =  [ "amount", "email", "tx_ref", "currency"]
 
         check_passed_parameters(required_parameters, data)
-        type = type
+        type = "enaira"
         payload = data.to_json
 
         response = post_request("#{base_url}#{BASE_ENDPOINTS::CHARGE_ENDPOINT}?type=#{type}", payload)
